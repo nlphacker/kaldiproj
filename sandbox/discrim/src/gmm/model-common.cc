d04357011b6788249dd51b571b59501e02b44873
danielpovey <danielpovey@5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8>
2012-02-02 06:32:39 +0000

    Final changes to lattice paper; other very minor issues.

---
+++ sandbox/discrim/src/gmm/model-common.cc
@@ -0,0 +1,88 @@
+// gmm/model-common.cc
+
+// Copyright 2009-2011  Microsoft Corporation
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+// WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+// See the Apache 2 License for the specific language governing permissions and
+// limitations under the License.
+
+#include "matrix/matrix-lib.h"
+#include "gmm/model-common.h"
+
+namespace kaldi {
+GmmFlagsType StringToGmmFlags(std::string str) {
+  GmmFlagsType flags = 0;
+  for (const char *c = str.c_str(); *c != '\0'; c++) {
+    switch (*c) {
+      case 'm': flags |= kGmmMeans; break;
+      case 'v': flags |= kGmmVariances; break;
+      case 'w': flags |= kGmmWeights; break;
+      case 't': flags |= kGmmTransitions; break;
+      case 'a': flags |= kGmmAll; break;
+      default: KALDI_ERR << "Invalid element " << CharToString(*c)
+                         << " of GmmFlagsType option string "
+                         << str;
+    }
+  }
+  return flags;
+}
+
+GmmFlagsType AugmentGmmFlags(GmmFlagsType flags) {
+KALDI_ASSERT((flags & ~kGmmAll) == 0);  // make sure only valid flags are present.
+  if (flags & kGmmVariances) flags |= kGmmMeans;
+  if (flags & kGmmMeans) flags |= kGmmWeights;
+  KALDI_ASSERT(flags & kGmmWeights);  // make sure zero-stats will be accumulated
+  return flags;
+}
+
+SgmmUpdateFlagsType StringToSgmmUpdateFlags(std::string str) {
+  SgmmUpdateFlagsType flags = 0;
+  for (const char *c = str.c_str(); *c != '\0'; c++) {
+    switch (*c) {
+      case 'v': flags |= kSgmmPhoneVectors; break;
+      case 'M': flags |= kSgmmPhoneProjections; break;
+      case 'w': flags |= kSgmmWeightProjections; break;
+      case 'S': flags |= kSgmmCovarianceMatrix; break;
+      case 'c': flags |= kSgmmSubstateWeights; break;
+      case 'N': flags |= kSgmmSpeakerProjections; break;
+      case 't': flags |= kSgmmTransitions; break;
+      case 'a': flags |= kSgmmAll; break;
+      default: KALDI_ERR << "Invalid element " << CharToString(*c)
+                         << " of SgmmUpdateFlagsType option string "
+                         << str;
+    }
+  }
+  return flags;
+}
+
+
+SgmmUpdateFlagsType StringToSgmmWriteFlags(std::string str) {
+  SgmmWriteFlagsType flags = 0;
+  for (const char *c = str.c_str(); *c != '\0'; c++) {
+    switch (*c) {
+      case 'g': flags |= kSgmmGlobalParams; break;
+      case 's': flags |= kSgmmStateParams; break;
+      case 'n': flags |= kSgmmNormalizers; break;
+      case 'u': flags |= kSgmmBackgroundGmms; break;
+      case 'a': flags |= kSgmmAll; break;
+      default: KALDI_ERR << "Invalid element " << CharToString(*c)
+                         << " of SgmmWriteFlagsType option string "
+                         << str;
+    }
+  }
+  return flags;
+}
+
+
+
+
+}  // End namespace kaldi
---
+++ sandbox/discrim/src/sgmmbin/sgmm-init.cc
@@ -0,0 +1,97 @@
+// sgmmbin/sgmm-init.cc
+
+// Copyright 2009-2011   Saarland University
+// Author:  Arnab Ghoshal
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+// WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+// See the Apache 2 License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/common-utils.h"
+#include "gmm/am-diag-gmm.h"
+#include "sgmm/am-sgmm.h"
+#include "hmm/transition-model.h"
+#include "tree/context-dep.h"
+
+
+int main(int argc, char *argv[]) {
+  try {
+    using namespace kaldi;
+    typedef kaldi::int32 int32;
+
+    const char *usage =
+        "Initialize an SGMM from a trained full-covariance UBM and a specified"
+        " model topology.\n"
+        "Usage: sgmm-init [options] <topology-in> <tree-in> <ubm-in> <sgmm-out>\n";
+    
+    bool binary = true;
+    int32 phn_space_dim = 0, spk_space_dim = 0;
+    kaldi::ParseOptions po(usage);
+    po.Register("binary", &binary, "Write output in binary mode");
+    po.Register("phn-space-dim", &phn_space_dim, "Phonetic space dimension.");
+    po.Register("spk-space-dim", &spk_space_dim, "Speaker space dimension.");
+
+
+    po.Read(argc, argv);
+    if (po.NumArgs() != 4) {
+      po.PrintUsage();
+      exit(1);
+    }
+
+    std::string topo_in_filename = po.GetArg(1),
+        tree_in_filename = po.GetArg(2),
+        ubm_in_filename = po.GetArg(3),
+        sgmm_out_filename = po.GetArg(4);
+
+    ContextDependency ctx_dep;
+    {
+      bool binary_in;
+      Input ki(tree_in_filename.c_str(), &binary_in);
+      ctx_dep.Read(ki.Stream(), binary_in);
+    }
+
+
+    HmmTopology topo;
+    {
+      bool binary_in;
+      Input ki(topo_in_filename, &binary_in);
+      topo.Read(ki.Stream(), binary_in);
+    }
+    
+    TransitionModel trans_model(ctx_dep, topo);    
+
+    kaldi::FullGmm ubm;
+    {
+      bool binary_read;
+      kaldi::Input is(ubm_in_filename, &binary_read);
+      ubm.Read(is.Stream(), binary_read);
+    }
+
+    kaldi::AmSgmm sgmm;
+    sgmm.InitializeFromFullGmm(ubm, trans_model.NumPdfs(), phn_space_dim,
+                               spk_space_dim);
+    sgmm.ComputeNormalizers();
+
+    {
+      kaldi::Output os(sgmm_out_filename, binary);
+      trans_model.Write(os.Stream(), binary);
+      sgmm.Write(os.Stream(), binary, kaldi::kSgmmWriteAll);
+    }
+
+    KALDI_LOG << "Written model to " << sgmm_out_filename;
+  } catch(const std::exception& e) {
+    std::cerr << e.what() << '\n';
+    return -1;
+  }
+}
+
+
